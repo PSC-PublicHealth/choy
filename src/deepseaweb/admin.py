44d27a28a5e74c98310e28da9e2746d6c950778d
Jay DePasse <depasse@psc.edu>
2018-11-07 11:21:33 -0500

    preserves last created object in dropdowns as default value

--- src/deepseaweb/admin.py
+++ src/deepseaweb/admin.py
@@ -48,10 +48,26 @@ class SampleAdmin(admin.ModelAdmin):
             'n_individuals',
             'tissuetype',
             )
-#    def __init__(self, *args, **kwargs):
-#        super().__init__(*args, **kwargs)
-#        origin_types = TblOriginTypes.objects.all()
-#        if len(origin_types) > 0:
-#            self.initial['origin_type'] = origin_types[len(origin_types)-1]
+    def add_view(self, request, form_url='', extra_context=None):
+        """ Customized add_view prepopulates admin fields with last created values for
+        select fields
+        """
+        data = request.GET.copy()
+
+        origin_types = TblOriginTypes.objects.all()
+        if len(origin_types) > 0:
+            data['origin_type'] = origin_types[len(origin_types)-1]
+
+        environmental_ids = TblEnvironmental.objects.all()
+        if len(environmental_ids) > 0:
+            data['environmentalid'] = environmental_ids[len(environmental_ids)-1]
+        
+        taxonomy_ids = TblTaxonomy.objects.all()
+        if len(taxonomy_ids) > 0:
+            data['taxonomyid'] = taxonomy_ids[len(taxonomy_ids)-1]
+
+        request.GET = data
+        return super().add_view(request, form_url=form_url, extra_context=extra_context)
+
 
 admin.site.register(TblSample, SampleAdmin)
